e110effd4d425f32d1bc3b1f3caa0f8ec432c792
S M Nazib Ul Alam <nazib9007.ice.bup@gmail.com>
2026-04-19 21:39:13 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-import greet, {add} from './problemSolve.js';
+import {add} from './problemSolve.js';
 console.log(add(5,5));
 
-//default import and Named import..
-import specialGreet from './problemSolve.js';
-console.log(specialGreet());
+//If when arise this type of sitiution like need to change the name of function then us " as " property..
+
+import {add as sum} from './problemSolve.js';
+console.log(sum(5,5));
+
+//Import default and Named function...
+import newNameFunction from './problemSolve.js';
+console.log(newNameFunctoin());
+
+//SO, that's the concept of import and export in javascript...
