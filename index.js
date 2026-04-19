fb938eaf25c8ad063c1e668893ee58f75df9febb
S M Nazib Ul Alam <nazib9007.ice.bup@gmail.com>
2026-04-19 21:31:35 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+import greet, {add} from './problemSolve.js';
+console.log(add(5,5));
+
+//default import and Named import..
+import specialGreet from './problemSolve.js';
+console.log(specialGreet());
