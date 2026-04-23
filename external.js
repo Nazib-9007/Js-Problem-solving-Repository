16b81cc74a99d8816485de37dcfc38c4b2adda14
S M Nazib Ul Alam <nazib9007.ice.bup@gmail.com>
2026-04-24 00:32:45 +0600

    Update external.js

--- external.js
+++ external.js
@@ -29,3 +29,45 @@ const givenName = greetUser('Nazib');
 
 let Name = document.querySelector('#message');
 Name.innerHTML = givenName;
+//Js previous topic problem solving...
+const subject = ['Algorithim', 'Data Structure', 'Javascript', 'Digital signal processing', 'signal system'];
+for(const key of subject){
+    console.log(key);
+}
+
+//
+const examDate = ['20-04-2026', '25-04-2026'];
+for(const keys of examDate){
+    console.log(keys);
+}
+
+let count = 0;
+while (count < 10) {
+    console.log('আমি প্রতিদিন মিনিমাম ৩ ঘন্টা করে কোড করি।।');
+    count++;
+}
+
+// 
+let number = 1;
+while (number < 21) {
+    console.log(number);
+    number++;
+}
+
+//
+
+let table = 1;
+while (table < 11) {
+    let multiplication = table * 3;
+    console.log(`${table} x 3 = ${multiplication}`);
+    table++;
+}
+
+//
+
+let divide = 100;
+while (divide < 111) {
+    let answer = divide / 2;
+    console.log(answer);
+    divide++;
+}
