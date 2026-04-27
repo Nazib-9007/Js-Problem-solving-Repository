a55a0e6ce975a9ee06ef522bbc33240dc26ac6a5
S M Nazib Ul Alam <nazib9007.ice.bup@gmail.com>
2026-04-28 00:03:07 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+//Array method map, filter, forEach, Find, FindINdex problem practice..
+const shopItem = [30, 45, 20,60,10];
+const increasePrice = shopItem.map(price => price + 13);
+console.log(increasePrice);
