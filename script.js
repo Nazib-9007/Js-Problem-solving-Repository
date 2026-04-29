e843bc4bb597a72a7f87443fd305b19d99dfc6a3
S M Nazib Ul Alam <nazib9007.ice.bup@gmail.com>
2026-04-29 20:29:42 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Array method map, filter, forEach, Find, FindINdex problem practice..
+//Array method map, filter, forEach, Find, FindINdex problem practice...
 //1..
 const shopItem = [30, 45, 20,60,10];
 const increasePrice = shopItem.map(price => price + 13);
