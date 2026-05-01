5ff00a25a325cd5bc3e5564baa667652aafcecbf
S M Nazib Ul Alam <nazib9007.ice.bup@gmail.com>
2026-05-02 01:21:39 +0600

    Update script.js

--- script.js
+++ script.js
@@ -137,3 +137,26 @@ const users = [
     {name: 'Tatti', age: 35}
 ];
 console.log(users.reverse());
+
+//Array method slice and splice problem solve...
+//1...
+const fruits = ['Apple', 'Banana', 'Cherry', 'Date'];
+console.log(fruits.slice(1,3));
+
+//2...
+const cars = ['Tesla', 'BMW', 'Toyota', 'Ford'];
+console.log(cars.slice(0,2));
+
+//3..
+const movieList = ['Inception', 'Titanic', 'Joker', 'Avatar', 'Interstellar'];
+console.log(movieList.splice(2,2,'Batman', 'Superman'));
+console.log(movieList);
+
+//4...
+const players = ['Messi', 'Ronaldo', 'Neymar', 'Mbappe'];
+console.log(players.splice(2,1, 'Halland'));
+console.log(players); 
+
+
+
+
