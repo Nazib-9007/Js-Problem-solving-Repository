0e4f346f184cccf545f144f9ee1e5a206fbd8cdf
S M Nazib Ul Alam <nazib9007.ice.bup@gmail.com>
2026-08-22 23:15:01 +0600

    Add files via upload
    Programming Hero 30 day's class..

---
+++ ph.js
@@ -0,0 +1,15 @@
+// const district = 'dhaka'
+// console.log(district.toUpperCase());
+
+// const disName = 'CUMILLAH'
+// console.log(disName.toLowerCase())
+
+// let mess = 'Hi! JavaScript. How are you?'
+// console.log(mess.includes('JavaScript'));
+// console.log(mess.length);
+
+// console.log(5 * 'Js')
+// console.log(5 * 0)
+// console.log(5 * '')
+
+console.log(typeof(undefined))
