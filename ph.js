501a71b6e469f98381055bab8c484f9c307abd51
S M Nazib Ul Alam <nazib9007.ice.bup@gmail.com>
2026-08-22 23:15:54 +0600

    Update ph.js
    Restored commented-out code and added new type checks for NaN and null.

--- ph.js
+++ ph.js
@@ -1,15 +1,16 @@
-// const district = 'dhaka'
-// console.log(district.toUpperCase());
+const district = 'dhaka'
+console.log(district.toUpperCase());
 
-// const disName = 'CUMILLAH'
-// console.log(disName.toLowerCase())
+const disName = 'CUMILLAH'
+console.log(disName.toLowerCase())
 
-// let mess = 'Hi! JavaScript. How are you?'
-// console.log(mess.includes('JavaScript'));
-// console.log(mess.length);
+let mess = 'Hi! JavaScript. How are you?'
+console.log(mess.includes('JavaScript'));
+console.log(mess.length);
 
-// console.log(5 * 'Js')
-// console.log(5 * 0)
-// console.log(5 * '')
-
+console.log(5 * 'Js')
+console.log(5 * 0)
+console.log(5 * '')
 console.log(typeof(undefined))
+console.log(typeof(NaN))
+console.log(typeof(null))
